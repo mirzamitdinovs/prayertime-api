66258381c8bcb1b5888740ca9102aae3e48922b6
mirzamitdinovs <sayyodmirzomitdinov@gmail.com>
2025-03-17 00:49:15 -0400

    scripts added

--- src/utils/contants.ts
+++ src/utils/contants.ts
@@ -1,9 +1,5 @@
 export const env = {
   PORT: Bun.env.PORT || 8080,
-  API_ID: Number(Bun.env.API_ID) || 0,
-  API_HASH: Bun.env.API_HASH || "",
-  CHANNEL_USERNAME: Bun.env.CHANNEL_USERNAME || "",
-  SESSION_STRING: Bun.env.SESSION_STRING || "",
   WEBSITE_URL: Bun.env.WEBSITE_URL || "https://islom.uz/vaqtlar",
   //
   API_VERSION: Bun.env.API_VERSION || "1.0.0",
